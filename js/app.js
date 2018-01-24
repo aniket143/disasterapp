2255d78d0ca8c8976a8337adf84edd0bb9c63d59
aniket143 <aniket143@gmail.com>
2018-01-24 17:49:10 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -69,15 +69,107 @@ $$('#my-login-screen .login-button').on('click', function () {
 });
 
 
-app.onPageInit('settings', function (page) {
+myApp.onPageInit('settings', function (page) {
+    // some stuff soon
+    var pictureSource;   // picture source
+    var destinationType; // sets the format of returned value
+
+    // Wait for device API libraries to load
+    //
+    document.addEventListener("deviceready",onDeviceReady,false);
+
+    // device APIs are available
+    //
+    function onDeviceReady() { 
+        pictureSource=navigator.camera.PictureSourceType;
+        destinationType=navigator.camera.DestinationType;
+    }
+
+    // Called when a photo is successfully retrieved
+    //
+    function onPhotoDataSuccess(imageData) {
+      // Uncomment to view the base64-encoded image data
+      // console.log(imageData);
+
+      // Get image handle
+      //
+      var smallImage = document.getElementById('smallImage');
+
+      // Unhide image elements
+      //
+      smallImage.style.display = 'block';
+
+      // Show the captured photo
+      // The in-line CSS rules are used to resize the image
+      //
+      smallImage.src = "data:image/jpeg;base64," + imageData;
+    }
+
+    // Called when a photo is successfully retrieved
+    //
+    function onPhotoURISuccess(imageURI) {
+      // Uncomment to view the image file URI
+      // console.log(imageURI);
+
+      // Get image handle
+      //
+      var largeImage = document.getElementById('largeImage');
+
+      // Unhide image elements
+      //
+      largeImage.style.display = 'block';
+
+      // Show the captured photo
+      // The in-line CSS rules are used to resize the image
+      //
+      largeImage.src = imageURI;
+    }
+
+    // A button will call this function
+    //
+    function capturePhoto() {
+        // Take picture using device camera and retrieve image as base64-encoded string
+        navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50,
+            destinationType: destinationType.DATA_URL });
+        //alert('yoo');
+    }
+
+    // A button will call this function
+    //
+    function capturePhotoEdit() {
+      // Take picture using device camera, allow edit, and retrieve image as base64-encoded string
+      navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 20, allowEdit: true,
+        destinationType: destinationType.DATA_URL });
+    }
+
+    // A button will call this function
+    //
     function getPhoto(source) {
-        // Retrieve image file location from specified source
-        navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50,
+      // Retrieve image file location from specified source
+      navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50,
         destinationType: destinationType.FILE_URI,
         sourceType: source });
     }
-        
-    $$('.change-profile-picture-btn').on('click', function () {
-        getPhoto();
+
+    // Called if something bad happens.
+    //
+    function onFail(message) {
+      alert('Failed because: ' + message);
+    }
+
+    $$('#capturePhoto').on('click', function () {
+        capturePhoto();
+    });
+
+    $$('#capturePhotoEdit').on('click', function () {
+        capturePhotoEdit();
+    });
+
+    $$('#photoLibrary').on('click', function () {
+        getPhoto(pictureSource.PHOTOLIBRARY);
+    });
+
+    $$('#photoAlbum').on('click', function () {
+        getPhoto(pictureSource.SAVEDPHOTOALBUM);
     });
 });
